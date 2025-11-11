1baca1d7dfe6f39160862d9306eef16bc6491417
dela528 <delalibenedicta1@gmail.com>
2025-11-11 15:18:08 +0000

    I uploaded a file

---
+++ question8.py
@@ -0,0 +1,14 @@
+score = float(input('Enter a score between 0 and 100:'))
+if score >= 90:
+    print('Grade A')
+elif score >= 80:
+    print('Grade B')
+elif score >= 70:
+    print('Grade C')
+elif score >= 60:
+    print('Grade D')
+elif score >= 50:
+    print('Grade E')
+elif score < 50:
+    print('Grade F')
+
